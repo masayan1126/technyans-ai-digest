3cd6f88d7020d50b15e6b3fb8f842466ecd4f584
masayan1126 <masa199311266@gmail.com>
2025-11-20 17:44:59 +0900

    Add responsive navigation breakpoints and update SVG assets
    - Introduced a new 'nav' screen size breakpoint at 1200px in the Tailwind configuration for improved responsive design.
    - Updated the Header component to utilize the new breakpoint for better visibility of navigation elements on larger screens.
    - Made extensive modifications to the Technyan SVG file, enhancing its structure and visual elements for better rendering and performance.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        'nav': '1200px',
+      },
       colors: {
         cream: '#FFF6D0',
         navy: {
